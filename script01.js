9bd58caff6f456fb7d218c65feca6dd6ec39f08f
aneperez <127619963+aneperez@users.noreply.github.com>
2023-11-18 17:58:04 -0300

    Add files via upload

---
+++ script01.js
@@ -0,0 +1,91 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//trabalhar com variáveis, console.log
+
+//utilizando padrão camelCase
+
+//variavel (let, var, const) -> identificador da variável > dados
+
+
+const carro = "carro"
+const fabricante_carro = "renault"
+const modeloCarro = "sandero"
+const corCarro = "vermelha"
+const anoCarro = 2010
+
+// saída de dados
+// carro: "carro"
+// fabricanteCarro: "renault"
+// modeloCarro: "sandero"
+// corCarro: "cor"
+// anoCarro: anoCarro
+
+
+// Primeira forma: Concatenação
+console.log ( "carro:" + carro + "fabricante:" + fabricante_carro + "modelo do carro" + modeloCarro +
+"cor do carro:" + corCarro + "ano:" + anoCarro );
+
+// Segunda forma: INterpolação
+console.log(`meu carro: ${carro}, fabricante do carro: ${fabricante_carro}, modelo carro: ${modeloCarro},
+ cor do carro: ${corCarro}, ano do carro: ${anoCarro}`);
+
+
+
+
+console.log ("carro");
+console.log ("Renault");
+console.log ("Sandero");
+console.log ("Vermelha");
+console.log (2010);
+
+
+
+********ATIVIDADE 01 ************
+//faça um programa que calcule a circunferência (área) de um círculo,
+//utilizando alguns conceitos, tais como,
+//variáveis, operadores aritméticos, concatenação, etc.
+//lembre-se da estrutura dos dados (Entrada de dados, processamento e saída de dados)
+//tendo em mente que o raio da circunferência é de R= 16
+
+
+1-entrada dados
+ 
+let PI= 3,14
+let RAIO= 16
+
+2-processamento
+π x R²
+PI  RAIO
+
+3-saída de dados
+LOG (resultado)
+
+
+
+
+
+
+
+
